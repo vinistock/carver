9ca2f76883d10036d840dd95fcc7480df774db64
Vinicius Stock <vinicius.stock@outlook.com>
2018-08-25 12:36:44 -0300

    Add benchmark support for profiler

--- lib/carver/profiler.rb
+++ lib/carver/profiler.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'benchmark'
+
 module Carver
   class Profiler
     def self.profile_memory(path, action, parent)
@@ -10,6 +12,18 @@ module Carver
       end
 
       presenter = Presenter.new(Result.new(report, :memory), path, action, parent)
+      presenter.log if Carver.configuration.log_results
+      presenter.add_to_results
+    end
+
+    def self.benchmark(path, action, parent)
+      return yield unless Carver.configuration.enabled
+
+      report = Benchmark.measure do
+        yield
+      end
+
+      presenter = Presenter.new(Result.new(report, :benchmark), path, action, parent)
       presenter.log if Carver.configuration.log_results
       presenter.add_to_results
     end
