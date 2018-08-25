210896eb7c6a39919ac87a86480e6ef21306d331
Vinicius Stock <vinicius.stock@outlook.com>
2018-08-25 13:05:03 -0300

    Address too many arguments infraction in profiler

--- lib/carver/profiler.rb
+++ lib/carver/profiler.rb
@@ -6,18 +6,18 @@ module Carver
   class Profiler
     def self.profile_memory(path, action, parent)
       return yield unless Carver.configuration.enabled
-      report = MemoryProfiler.report { yield }
-      present_results(report, path, action, parent, :memory)
+      @report = MemoryProfiler.report { yield }
+      present_results(path, action, parent, :memory)
     end
 
     def self.benchmark(path, action, parent)
       return yield unless Carver.configuration.enabled
-      report = Benchmark.measure { yield }
-      present_results(report, path, action, parent, :benchmark)
+      @report = Benchmark.measure { yield }
+      present_results(path, action, parent, :benchmark)
     end
 
-    def self.present_results(report, path, action, parent, type)
-      presenter = Presenter.new(Result.new(report, type), path, action, parent)
+    def self.present_results(path, action, parent, type)
+      presenter = Presenter.new(Result.new(@report, type), path, action, parent)
       presenter.log if Carver.configuration.log_results
       presenter.add_to_results
     end
