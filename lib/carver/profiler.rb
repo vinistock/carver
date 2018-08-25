5031875068a29755f34cd71f88e0946b99eccb9f
Vinicius Stock <vinicius.stock@outlook.com>
2018-08-25 12:45:43 -0300

    Address code duplication in profiler

--- lib/carver/profiler.rb
+++ lib/carver/profiler.rb
@@ -6,24 +6,18 @@ module Carver
   class Profiler
     def self.profile_memory(path, action, parent)
       return yield unless Carver.configuration.enabled
-
-      report = MemoryProfiler.report do
-        yield
-      end
-
-      presenter = Presenter.new(Result.new(report, :memory), path, action, parent)
-      presenter.log if Carver.configuration.log_results
-      presenter.add_to_results
+      report = MemoryProfiler.report { yield }
+      present_results(report, path, action, parent, :memory)
     end
 
     def self.benchmark(path, action, parent)
       return yield unless Carver.configuration.enabled
+      report = Benchmark.measure { yield }
+      present_results(report, path, action, parent, :benchmark)
+    end
 
-      report = Benchmark.measure do
-        yield
-      end
-
-      presenter = Presenter.new(Result.new(report, :benchmark), path, action, parent)
+    def self.present_results(report, path, action, parent, type)
+      presenter = Presenter.new(Result.new(report, type), path, action, parent)
       presenter.log if Carver.configuration.log_results
       presenter.add_to_results
     end
